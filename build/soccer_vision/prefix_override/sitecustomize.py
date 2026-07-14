c5fe931da3950c616237b7e42e14e5ffce01b341
hasanshahriar32 <hasanshahriar32@gmail.com>
2026-07-14 10:00:33 +0600

    chore: add gitignore and remove build artifacts

--- build/soccer_vision/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/sharmin/Desktop/iot/soccer_bot/install/soccer_vision'
--- build/ydlidar_sdk/python/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sharmin/Desktop/iot/soccer_bot/src/YDLidar-SDK/python
-# Build directory: /home/sharmin/Desktop/iot/soccer_bot/build/ydlidar_sdk/python
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(ydlidar_py_test "/usr/bin/python3" "/home/sharmin/Desktop/iot/soccer_bot/src/YDLidar-SDK/python/test/pytest.py")
-set_tests_properties(ydlidar_py_test PROPERTIES  ENVIRONMENT "PYTHONPATH=/opt/ros/jazzy/lib/python3.12/site-packages:/home/sharmin/Desktop/iot/soccer_bot/build/ydlidar_sdk/python" _BACKTRACE_TRIPLES "/home/sharmin/Desktop/iot/soccer_bot/src/YDLidar-SDK/python/CMakeLists.txt;42;add_test;/home/sharmin/Desktop/iot/soccer_bot/src/YDLidar-SDK/python/CMakeLists.txt;0;")
-subdirs("examples")
